35d6e77b169de3b04f60240c6ee4831596d1263d
Mehul Purohit <mehulpurohit000@gmail.com>
2023-08-03 11:15:13 +0530

    added script to send email

---
+++ main.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+
+subject = "Hello mehul"
+body = "Ujbjkbjkb nb jbkj"
+sender = "mehulpurohit000@gmail.com"
+recipients = ["mehulpurohit23@gmail.com"]
+password = "rixicyjmafejyfbi"
+
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+
+send_email(subject, body, sender, recipients, password)
